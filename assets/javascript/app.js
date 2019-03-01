b1b0c467e26e257fadae800683ad12173b028b5a
Samuel Yu <yusungsamuel@Samuels-MacBook-Pro-2.local>
2019-02-28 22:40:19 -0800

    add image for when answer is display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     //Grabbing HTML index 
     var questionText = $("#question")
     var answerButton1 = $("#answer-button-1");
@@ -10,6 +11,8 @@ $(document).ready(function () {
     var answerButton = $(".answer-button")
     var correctText = $("#correct")
     var incorrectText = $("#incorrect")
+    var video = $("#video")
+    var scoreboardText = $("#scoreboard")
 
     //Global Variables
     var correct = 0;
@@ -19,127 +22,145 @@ $(document).ready(function () {
     var questionCounter = 0
     var timer;
     var time = 30
-    var correctAnswerArr = ["Miley Cyrus", "All I Want For Christmas Is You", "Kourtney Kardashian","0", "Nick Jonas", "Peter Hernandez", "Kendall Jenner", "Tommy Hilfiger", "Kelly Clarkson", "Tyra Banks"]
+    var correctAnswerArr = ["Miley Cyrus", "All I Want For Christmas Is You", "Kourtney Kardashian", "0", "Peter Hernandez", "Kelly Rowland","Kendall Jenner", "Little Monster", "Kelly Clarkson", "Tyra Banks"]
     //A Bank where all the question and answer pair are stored.    
     var questionBank = [
         {
             question: "Which on these female singer did not participate in the song 'Bang Bang'?",
-            answer: ["Ariana Grande", "Jessie J", "Nicki Minaj", "Miley Cyrus"]
+            answer: ["Ariana Grande", "Jessie J", "Nicki Minaj", "Miley Cyrus"],
+            image: "https://gifimage.net/wp-content/uploads/2017/09/bang-bang-bang-gif-11.gif"
         },
         {
             question: "Which Mariah Carey's song is not a billboard #1 hit?",
-            answer: ["We Belong Together", "All I Want For Christmas Is You", "Always Be My Baby", "Vision of Love"]
+            answer: ["We Belong Together", "All I Want For Christmas Is You", "Always Be My Baby", "Vision of Love"],
+            image: "https://media.vanityfair.com/photos/57d1f0fc5475d07256a8b2d9/master/w_690,c_limit/taylor-swift-britney-spears-i-dont-know-her-04.gif"
         },
         {
             question: "Which of the Kardashian is the oldest?",
-            answer: ["Kim Kardashian", "Khole Kardashian", "Kourtney Kardashian", "Rob Kardashian"]
+            answer: ["Kim Kardashian", "Khole Kardashian", "Kourtney Kardashian", "Rob Kardashian"],
+            image: "https://media.giphy.com/media/XDYCYqXqV8VX2/giphy.gif"
         },
         {
-            question: "How many Grammy Awards has singer, Katy Perrt, won?",
-            answer: ["0", "1", "2", "3"]
+            question: "How many Grammy Awards has singer, Katy Perry, won?",
+            answer: ["0", "1", "2", "3"],
+            image: "https://media0.giphy.com/media/u7DzdWhEbXfUs/giphy.gif"
         },
         {
-            question: "Which of these male celebrity have Taylor Swift not dated?",
-            answer: ["Harry Styles", "Calvin Harris", "John Mayer", "Nick Jonas"]
+            question: "What is Bruno Mars's birthname?",
+            answer: ["Roger Garcia", "Peter Hernandez", "Bruno Mars", "Edward Rodriguez"],
+            image: "https://media.giphy.com/media/3o7TKr77iosHAxJmUM/giphy.gif"
         },
         {
-            question: "What is Bruno Mars's birhtname?",
-            answer: ["Roger Garcia", "Peter Hernandez", "Bruno Mars", "Edward Rodriguez"]
-        },
-        {
-            question: "Which one of these female singer did not participate in the 2006 movie Dreamgirls?",
-            answer: ["Beyonce Knowles", "Jennifer Hudson", "Kelly Rowland", "Anika Noni Rose"]
+            question: "Which one of these female singer did not participate in the 2006 movie, Dreamgirls?",
+            answer: ["Beyonce Knowles", "Jennifer Hudson", "Kelly Rowland", "Anika Noni Rose"],
+            image: "https://media.giphy.com/media/mrTkACKkXyUiA/giphy.gif"
         },
         {
             question: "Which of these female model was not a cast in Taylor Swift's Bad Blood music video?",
-            answer: ["Gigi Hadid", "Kendall Jenner", "Karlie Klos", "Cara Delevingne"]
+            answer: ["Gigi Hadid", "Kendall Jenner", "Karlie Klos", "Cara Delevingne"],
+            image: "https://media.giphy.com/media/1k2VgNODsyUtXqSYz3/giphy.gif"
         },
         {
-            question: "Which of these male designer is not gay?",
-            answer: ["Tom Ford", "Yves Saint Laurent", "Stefano Gabbana", "Tommy Hilfiger"]
+            question: "What do Lady Gaga call her fans?",
+            answer: ["Little Monster", "Little Creature", "Little Freak", "Little Beast"],
+            image: "https://media.giphy.com/media/yZsfsDAiQvhxS/giphy.gif"
         },
         {
             question: "Which of these singer was not a micky mouse club singer?",
-            answer: ["Kelly Clarkson","Christina Aguilera", "Britney Spears", "Justin Timberlake"]
-        }, 
+            answer: ["Kelly Clarkson", "Christina Aguilera", "Britney Spears", "Justin Timberlake"],
+            image:  "https://media.giphy.com/media/6nYNPwSUsExLGeFBMz/giphy.gif"
+        },
         {
             question: "Which model created the tv show, America's Next Top Model?",
-            answer: ["Tyra Banks", "Naomi Campbell", "Kate Moss", "Cindy Crawford"]
+            answer: ["Tyra Banks", "Naomi Campbell", "Kate Moss", "Cindy Crawford"],
+            image: "https://media0.giphy.com/media/UyH4vj65PQseY/giphy.gif?cid=3640f6095c665b834942545641df99c1"
         }
     ]
 
-    
+
 
     //Global Functions
-    function displayTimer () {
-        timerText.text(time)
-        time -- 
-        if (time === 0){
-            time = 30
-        }    
+    function resetBetweenQuestions() {
+        clearInterval(showQuestion)
+        time = 30;
+        nextQuestion();
     }
-    
-    function startGame () {
-        if (!gameStarted){
+
+    function displayTimer() {
+        timerText.text(time)
+        time--
+        if (time <= 0) {
+            time = 30
+        }
+    }
+
+    function startGame() {
+        if (!gameStarted) {
             gameStarted = true
             displayQuestion()
             displayTimer()
             showQuestion = setInterval(nextQuestion, 30000)
-            timer = setInterval(displayTimer,1000)
+            timer = setInterval(displayTimer, 1000)
         }
     }
 
-    function nextQuestion () {
-        if(gameStarted){
-        questionCounter ++ ;
-        displayQuestion() 
-        displayTimer() 
-        setTimeout(displayQuestion, 1000)}
-        else{
-            return
-        } 
-        if (questionCounter === questionBank.length-1){
+    function nextQuestion() {
+        questionCounter++
+        if (questionCounter > questionBank.length-1) {
             gameStarted = false
             clearInterval(showQuestion)
-        }
-    }
-
-    function displayQuestion (){
-        questionText.text(questionBank[questionCounter].question);
-        answerButton1.text(questionBank[questionCounter]["answer"][0]);
-        answerButton1.attr("value",questionBank[questionCounter]["answer"][0]);
-        answerButton2.text(questionBank[questionCounter]["answer"][1]);
-        answerButton2.attr("value",questionBank[questionCounter]["answer"][1])
-        answerButton3.text(questionBank[questionCounter]["answer"][2]);
-        answerButton3.attr("value",questionBank[questionCounter]["answer"][2])
-        answerButton4.text(questionBank[questionCounter]["answer"][3]); 
-        answerButton4.attr("value",questionBank[questionCounter]["answer"][3])
-
-    }
-//The two commented out codes below need more work
-    answerButton.on("click", compareCorrectAnswer)
-
-    function compareCorrectAnswer (){
-        var value = $(this).attr("value");  
-        if (correctAnswerArr.indexOf(value) === -1) {
-            incorrect ++
-            incorrectText.text("Incorrect: " + incorrect)
-            console.log(correctAnswerArr.indexOf(value))
-            console.log(value)
-            time = 30;
-            nextQuestion()
+            clearInterval(timer)
+            scoreboardText.text("Correct: " + correct + "  " + "Incorrect: " + incorrect)
+        };
+        if (gameStarted) {
+            showQuestion = setInterval(nextQuestion, 30000)
+            displayQuestion()
+            displayTimer()
+            // setTimeout(displayQuestion, 1000)
         }
         else {
-            correct ++
-            correctText.text("Correct: " + correct);
-            console.log(correctAnswerArr.indexOf(value));  
-            time = 30;
-            nextQuestion();          
+            return
+        }
+        
+    }
+
+    function displayQuestion() {
+        questionText.text(questionBank[questionCounter].question);
+        answerButton1.text(questionBank[questionCounter]["answer"][0]);
+        answerButton1.attr("value", questionBank[questionCounter]["answer"][0]);
+        answerButton2.text(questionBank[questionCounter]["answer"][1]);
+        answerButton2.attr("value", questionBank[questionCounter]["answer"][1])
+        answerButton3.text(questionBank[questionCounter]["answer"][2]);
+        answerButton3.attr("value", questionBank[questionCounter]["answer"][2])
+        answerButton4.text(questionBank[questionCounter]["answer"][3]);
+        answerButton4.attr("value", questionBank[questionCounter]["answer"][3])
+
+    }
+    //The two commented out codes below need more work
+    answerButton.on("click", compareCorrectAnswer)
+
+    function compareCorrectAnswer() {
+        var value = $(this).attr("value");
+        if (gameStarted) {
+            if (correctAnswerArr.indexOf(value) === -1) {
+                incorrect++
+                incorrectText.text("Incorrect: " + incorrect)
+                console.log(correctAnswerArr.indexOf(value))
+                console.log(questionCounter)
+                resetBetweenQuestions()
+            }
+            else {
+                correct++
+                correctText.text("Correct: " + correct);
+                console.log(correctAnswerArr.indexOf(value));
+                console.log(questionCounter)
+                resetBetweenQuestions()
+            }
         }
     }
-    
-    
-    startButton.click(startGame) 
+
+    console.log(correctAnswerArr.length)
+    startButton.click(startGame)
 })
 
 
